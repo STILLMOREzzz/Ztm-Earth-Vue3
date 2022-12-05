89d6e0f60e96a1684518d73accfceffdd553ec0c
STILLMOREzzz <15226599619@163.com>
2022-12-05 21:08:29 +0800

    feat(less):add less support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import proxy from './src/utils/proxy';
+import { VITE_PORT } from './src/utils/constant';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +13,24 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          hack: `true; @import (reference) "${path.resolve("src/assets/styles/base.less")}";`,
+        },
+        javascriptEnabled: true,
+      },
+    },
+  },
+  server: {
+    hmr: { overlay: false }, // 禁用或配置 HMR 连接 设置 server.hmr.overlay 为 false 可以禁用服务器错误遮罩层
+    // 服务配置
+    port: VITE_PORT, // 类型： number 指定服务器端口;
+    open: false, // 类型： boolean | string在服务器启动时自动在浏览器中打开应用程序；
+    cors: false, // 类型： boolean | CorsOptions 为开发服务器配置 CORS。默认启用并允许任何源
+    host: '0.0.0.0', // IP配置，支持从IP启动
+    proxy,
+  },
 })
