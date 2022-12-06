5acc5a457437addd7a99618cce2804c5e50f7a94
STILLMOREzzz <15226599619@163.com>
2022-12-06 16:41:25 +0800

    feat(router): add router support

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import Visual from '@/views/visual/index.vue'
 
+const routerHistory = createWebHashHistory(import.meta.env.BASE_URL)
+// createWebHashHistory hash 路由
+// createWebHistory history 路由
+// createMemoryHistory 带缓存 history 路由
+
+export const constantRoutes = [
+  {
+    path: '/',
+    name: 'home',
+    redirect: '/visual',
+    children: [
+      {
+        path: '/visual',
+        name:'visual',
+        component: Visual,
+      }
+    ]
+  }
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    {
-      path: '/visual',
-      name:'visual',
-      component: Visual,
-    }
-  ]
+  history: routerHistory,
+  routes: constantRoutes
 })
+
+// 删除/重置路由
+export function resetRoute() {
+  router.getRoutes().forEach((route) => {
+    const { name } = route
+    if (name) {
+      router.hasRoute(name) && router.removeRoute(name)
+    }
+  })
+}
+
+export function setupRouter(app){
+  app.use(router)
+}
 
 export default router
