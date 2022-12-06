60ab297770192e6254e6b22207b396759e371d7a
STILLMOREzzz <15226599619@163.com>
2022-12-06 16:34:37 +0800

    feat(element): Add element-plus

--- src/main.js
+++ src/main.js
@@ -2,13 +2,28 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import router, { setupRouter } from './router' // 路由
+import { setupElementPlus } from './libs/element' // element UI
+import { setupDirective } from './libs/directive/index'
+import { setupGlobalCom } from './components/index'
 
 import './assets/styles/main.less'
+import mitt from 'mitt';
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+setupRouter(app) // 引入路由
 
-app.mount('#app')
+setupElementPlus(app) // 引入element组件
+
+setupDirective(app) // 注册全局指令
+
+setupGlobalCom(app) // 注册全局公用组件
+
+app.use(createPinia())
+
+app.config.globalProperties.mittBus = mitt()
+
+router.isReady().then(() => {
+  app.mount('#app')
+})
