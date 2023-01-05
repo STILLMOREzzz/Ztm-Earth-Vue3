fe07b0f2a2a77cfd051b0e3ff7f5b7f0d94f3b0e
STILLMOREzzz <15226599619@163.com>
2023-01-05 23:25:06 +0800

    docs(readme): update the readme file

--- src/stores/modules/layer.js
+++ src/stores/modules/layer.js
@@ -3,7 +3,7 @@
  * @Date: 2023-01-01
  * @Description: 图层数据存储
  * @LastEditors: STILLMOREzzz
- * @LastEditTime: 2023-01-04 21:23
+ * @LastEditTime: 2023-01-05 14:20
  * @FilePath: ztm-earth-vue3/src/stores/modules/layer.js
  */
 import { defineStore } from "pinia";
